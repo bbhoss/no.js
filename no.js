29f47830aa7e495a94152af200e7fdfb7552b4fc
Preston Marshall <preston.marshall@complymd.com>
2014-10-02 16:11:46 -0500

    Add yes and maybe functions
    Add yes and maybe functions to support enterprise applications.

--- no.js
+++ no.js
@@ -1,5 +1,15 @@
+"use strict";
 (function() {
   Object.prototype.no = function() {
     return false && 0 && "" && null && undefined && NaN && !true;
+  }
+  
+  Object.prototype.yes = function() {
+    return arguments.callee.caller.no(arguments);
+  }
+  
+  Object.prototype.maybe = function() {
+    var trill = new Date()
+    return !!(trill.getTime() % 2 === 0);
   }
 })();
